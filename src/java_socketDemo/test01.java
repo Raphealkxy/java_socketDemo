1e919ec71197d26ba8b14e2602413e81b540574f
Raphealkxy <1277609958@qq.com>
2017-06-17 13:28:15 +0800

    add the code in test01

--- src/java_socketDemo/test01.java
+++ src/java_socketDemo/test01.java
@@ -1,10 +1,39 @@
 package java_socketDemo;
 
+import java.lang.reflect.Array;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+
 public class test01 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws UnknownHostException{
+		//获取本机的InetAddress实例对象
+          InetAddress address=InetAddress.getLocalHost();
+          System.out.println("主机名是："+address.getHostName());
+          System.out.println("主机地址是："+address.getHostAddress());
+          byte [] bytes=address.getAddress();
+          System.out.println(Arrays.toString(bytes));
+          System.out.println(address);
+          
+          //根据机器名获取InetAddress实例对象
+          InetAddress address2=InetAddress.getByName("Timmy-Kxy");
+          System.out.println("主机名是："+address.getHostName());
+          System.out.println("主机地址是："+address.getHostAddress());
+          byte [] bytes1=address.getAddress();
+          System.out.println(Arrays.toString(bytes1));
+          System.out.println(address);
+          
+          //根据IP地址获取本机InetAdress对象
+          InetAddress address3=InetAddress.getByName("10.49.198.223");
+          System.out.println("主机名是："+address.getHostName());
+          System.out.println("主机地址是："+address.getHostAddress());
+          byte [] bytes2=address.getAddress();
+          System.out.println(Arrays.toString(bytes2));
+          System.out.println(address);
+          
+          
+          
 	}
 
 }
